25384cf113899f30c68c20e0c26479c391cb8258
Ian O'Donnell <iano@id.iit.edu>
2017-05-04 16:13:44 -0500

    added success function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $.ajax({
     url: `https://quarkbackend.com/getfile/karbassi/user-profiles`,
-
+    type: "GET",
+    success: function(response) {
+        for (var i = 0; i < response.length; i++) {
+            var user = response[i];
+        }
+    }
 });
