e04a052c4ba2402be2493f2ab3ea52aed815668a
Ian O'Donnell <iano@id.iit.edu>
2017-05-04 19:09:24 -0500

    got company name and address

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ function updateUserInfo(index, user) {
     var $user = $(".user-profile").eq(index);
 
     $user.find(".thumbnail").find("img").attr("src", user.picture);
-        $user.find(".caption").find("h3").text(user.name.first + " " + user.name.last);
+    $user.find(".caption").find("h3").text(user.name.first + " " + user.name.last);
+    $user.find(".company").find(".name").text(user.company.name);
+    $user.find(".company").find(".street").text(user.company.address.street);
+    $user.find(".company").find(".city").text(user.company.address.city);
+    $user.find(".company").find(".state").text(user.company.address.state);
+    $user.find(".company").find(".zip").text(user.company.address.zip);
 
 
 }
