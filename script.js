1bb0bee46266f99cec7ee764771be380b5cdf908
Ian O'Donnell <iano@id.iit.edu>
2017-05-04 19:06:35 -0500

    got images

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $.ajax({
     url: `https://quarkbackend.com/getfile/karbassi/user-profiles`,
     type: "GET",
-    success: function(response) {
+    success: function (response) {
         for (var i = 0; i < response.length; i++) {
             var user = response[i];
+            updateUserInfo(i, user);
         }
     }
 });
+
+function updateUserInfo(index, user) {
+    var $user = $(".user-profile").eq(index);
+
+    $user.find(".thumbnail").find("img").attr("src", user.picture);
+
+
+}
